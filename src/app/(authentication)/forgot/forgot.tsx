f92020fc17a4aff141eaa4d80ca481c26367a1e7
Dev <Johnstar0143@gmail.com>
2024-05-08 12:27:51 -0400

    updated config

--- src/app/(authentication)/forgot/forgot.tsx
+++ src/app/(authentication)/forgot/forgot.tsx
@@ -1,51 +1,50 @@
 'use client'
-
 import {
   Alert, Button, Form, FormControl, InputGroup,
 } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
-import { useRouter } from 'next/navigation'
-import { SyntheticEvent, useState } from 'react'
-import { deleteCookie, getCookie } from 'cookies-next'
-import axios from 'axios'
+// import { faLock } from '@fortawesome/free-solid-svg-icons'
+// import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+// import { deleteCookie, getCookie } from 'cookies-next'
+// import axios from 'axios'
 import InputGroupText from 'react-bootstrap/InputGroupText'
 
 export default function Forgot() {
-  const router = useRouter()
-  const [submitting, setSubmitting] = useState(false)
+  // const router = useRouter()
+  // const [submitting, setSubmitting] = useState(false)
   const [error, setError] = useState('')
 
-  const getRedirect = () => {
-    const redirect = getCookie('redirect')
-    if (redirect) {
-      deleteCookie('redirect')
-      return redirect.toString()
-    }
+  // const getRedirect = () => {
+  //   const redirect = getCookie('redirect')
+  //   if (redirect) {
+  //     deleteCookie('redirect')
+  //     return redirect.toString()
+  //   }
 
-    return '/'
-  }
+  //   return '/'
+  // }
 
-  const register = async (e: SyntheticEvent) => {
-    e.stopPropagation()
-    e.preventDefault()
+  // const register = async (e: SyntheticEvent) => {
+  //   e.stopPropagation()
+  //   e.preventDefault()
 
-    setSubmitting(true)
+  //   setSubmitting(true)
 
-    try {
-      const res = await axios.post('api/mock/login')
-      if (res.status === 200) {
-        router.push(getRedirect())
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message)
-      }
-    } finally {
-      setSubmitting(false)
-    }
-  }
+  //   try {
+  //     const res = await axios.post('api/mock/login')
+  //     if (res.status === 200) {
+  //       router.push(getRedirect())
+  //     }
+  //   } catch (err) {
+  //     if (err instanceof Error) {
+  //       setError(err.message)
+  //     }
+  //   } finally {
+  //     setSubmitting(false)
+  //   }
+  // }
 
   return (
     <>
@@ -59,13 +58,13 @@ export default function Forgot() {
             type="email"
             name="email"
             required
-            disabled={submitting}
+            disabled={false}
             placeholder="Email"
             aria-label="Email"
           />
         </InputGroup>
 
-        <Button type="submit" className="d-block w-100" disabled={submitting} variant="success">
+        <Button type="submit" className="d-block w-100" disabled={false} variant="success">
           Send link
         </Button>
       </Form>
